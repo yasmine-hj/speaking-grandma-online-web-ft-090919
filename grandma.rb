5cd9558fd6d242b38f96f1dd69da628a0d470b18
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-09 21:52:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,7 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  end
-  if phrase != phrase.upcase()
+  elsif phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
